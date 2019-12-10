c1bb5810340af6b7ce26c0310c9b8fcfb013f357
Adrian Olszowski <aol@spyro-soft.com>
2019-12-10 19:51:21 +0100

    carousela photo and styles

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import ItemsCarousel from 'react-items-carousel';
 import range from 'lodash/range';
+import photo0 from '../../public/images/photo0.png'
+import photo1 from '../../public/images/photo1.png'
+import photo2 from '../../public/images/photo2.png'
+import { useBodyStyles } from '../styles/styles';
 
 
-export default function MyCarousel() {
+export default function MyCarousel(props) {
+    const classes = useBodyStyles(props.theme)
     
     const [activeItemIndex, setActioveItemIndex] = useState<number>(0);
     const [children, setChildren] = useState<any[]>([]);
     
-    const createChildren = (n: number) => range(n).map((i: number) => <div key={i} style={{ height: 200, background: '#333' }}>{i}</div>);
+    const createChildren = (n: number) => range(n).map((i: number) => <img src={`../../public/images/photo${i}.png`} style={{width: '200px'}}  className={classes.paper}/>);
     // 
     useEffect(() => {
-        setChildren(createChildren(20));
+        setChildren(createChildren(3));
     }, [])
     const changeActiveItem = (activeItemIndex: number) => setActioveItemIndex(activeItemIndex)
     return (
